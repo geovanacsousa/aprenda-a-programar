d48c0458381e824ab50b3b0d0aaeaec2f0711367
Geovana Sousa <geovanaaggcs@gmail.com>
2022-08-30 20:36:40 -0300

    pratica js 2

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,10 @@ let email = "meuemail"
 let profissao = "analista de bussines inteligence"
 let idade = 18
 let dreamcity = "New York" 
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(dreamcity)
+
